eaf08145b8e660bfba5d0fe9df2ce914a843d670
GustavoRV08 <gustavorv102@gmail.com>
2026-06-14 12:16:57 -0300

    só consertando alguns erros de digitação
    Corrected spelling and grammar in task strings.

--- Projeto CG Síntese de Imagem/texto.py
+++ Projeto CG Síntese de Imagem/texto.py
@@ -3,28 +3,28 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
 tarefas = [
-      "Já está tudo pronto, agota é só não olhar para trás",
+      "Já está tudo pronto, agora é só não olhar para trás",
       "Essa será minha última conversa com aquela agiota",
-      "Ouve um incendio no centro recentemente, foi uma tragédia",
+      "Houve um incêndio no centro recentemente, foi uma tragédia",
       "Eu odeio pessoas que se atrasam, elas não se importam com o tempo de vida dos outros",
       "Tive um resfriado muito forte semana passada, não quero ter outro"
   ]
 
 def afazeres():
   global tarefas
-  
+
   y = 0.9
 
   glColor3f(1, 1, 1)
   glRasterPos2f(-0.93, y)
-  
+
   if len(tarefas) > 0:
     for char in tarefas[-1]:
           glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ord(char))
   else:
     for char in "SEJA NAPOLEÃO!":
           glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ord(char))
-    
+
 
 
 def proxima_tarefa():
@@ -34,9 +34,9 @@ def proxima_tarefa():
     tarefa_atual = 0
 
 tarefas = [
-      "Já está tudo pronto, agota é só não olhar para trás",
+      "Já está tudo pronto, agora é só não olhar para trás",
       "Essa será minha última conversa com aquela agiota",
-      "Ouve um incendio no centro recentemente, foi uma tragédia",
+      "Houve um incêndio no centro recentemente, foi uma tragédia",
       "Pessoas que se atrasam não se importam com o tempo de vida dos outros",
       "Tive um resfriado muito forte semana passada, não quero ter outro"
   ]
